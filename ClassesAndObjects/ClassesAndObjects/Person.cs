07c99928d489677a574f5cbe3037fc9cfa2b5883
John Swan <johneswan3@protonmail.com>
2022-06-18 17:32:55 -0600

    Initial commit

---
+++ ClassesAndObjects/ClassesAndObjects/Person.cs
@@ -0,0 +1,40 @@
+// Primitive Datatypes
+/*
+ * int, string, char, bool, etc
+ */
+ 
+// Object Oriented Programming - Creating your own datatypes
+
+/*
+ * Class - Blueprint
+ */
+// Single Responsibility Principle
+// Let each unit have 1 responsibility
+
+
+public class Person
+{
+    // Properties when public
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    
+    // Fields when private
+    private double _salary { get; set; }
+    
+    // Method to set private salary
+    public void setSalary(double salary)
+    {
+        _salary = salary;
+    }
+    // Method to get private salary
+    public double getSalary() // Must be same data type as field
+    {
+        return _salary;
+    }
+
+    public string getFullName()
+    {
+        return $"{FirstName} {LastName}";
+    }
+}
---
+++ ClassesAndObjects/ClassesAndObjects/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Object Declaration
+Person person = new();
+
+Console.WriteLine("Enter First Name:");
+person.FirstName = Console.ReadLine();
+
+Console.WriteLine("Enter Last Name:");
+person.LastName = Console.ReadLine();
+
+Console.WriteLine("Enter Age:");
+person.Age = Convert.ToInt32(Console.ReadLine());
+
+// Get Salary from input and assign it to the Person class
+Console.WriteLine("Enter Salary:");
+double salary = Convert.ToInt32(Console.ReadLine());
+person.setSalary(salary);
+
+// Console.WriteLine($"First Name: {person.FirstName}");
+// Console.WriteLine($"Last Name: {person.LastName}");
+Console.WriteLine($"Name: {person.getFullName()}");
+Console.WriteLine($"Age: {person.Age}");
+Console.WriteLine($"Salary: {person.getSalary()}");
